7251d2328d8d115265c95526158e7e43e509c856
Carol-gutianle <2311601671@qq.com>
2025-03-10 14:11:45 +0000

    upload: openrlhf

---
+++ openrlhf/models/mm_actor.py
@@ -0,0 +1,78 @@
+from typing import Optional, Tuple, Union
+
+import torch
+import torch.distributed as dist
+import torch.nn as nn
+from flash_attn.utils.distributed import all_gather
+from peft import LoraConfig, TaskType, get_peft_model
+from peft.tuners.lora import LoraLayer
+from torch.nn import functional as F
+from transformers import Qwen2_5_VLForConditionalGeneration, AutoProcessor
+from transformers.integrations.deepspeed import HfDeepSpeedConfig
+
+from .ring_attn_utils import convert_ring_attn_params
+from .utils import log_probs_from_logits, reset_position_ids
+
+class MMActor(nn.Module):
+    """
+    Base class for Actor models in reinforcement learning.
+
+    This class serves as a foundation for implementing various actor models, which are responsible for selecting actions based on the policy learned from the environment.
+
+    Args:
+        pretrain_or_model (nn.Module): A pretrained model or a new model instance to be used as the actor.
+        use_flash_attention_2 (bool, optional): Whether to utilize Flash Attention 2.0 for improved performance. Defaults to False.
+        bf16 (bool, optional): Enable bfloat16 precision for model computations. Defaults to True.
+        load_in_4bit (bool, optional): Load the model in 4-bit precision. Defaults to False.
+        lora_rank (int, optional): Rank for LoRA adaptation. Defaults to 0.
+        lora_alpha (int, optional): Alpha parameter for LoRA. Defaults to 16.
+        lora_dropout (float, optional): Dropout rate for LoRA layers. Defaults to 0.
+        target_modules (list, optional): List of target modules for applying LoRA. Defaults to None.
+        ds_config (dict, optional): Configuration for DeepSpeed, enabling model partitioning across multiple GPUs. Defaults to None.
+        device_map (dict, optional): Device mapping for loading the model onto specific devices. Defaults to None.
+        packing_samples (bool, optional): Whether to pack samples during training. Defaults to False.
+    """
+    
+    def __init__(
+        self,
+        pretrain_or_model,
+        use_flash_attention_2=False,
+        bf16=True,
+        load_in_4bit=False,
+        lora_rank=0,
+        lora_alpha=16,
+        lora_dropout=0,
+        target_modules=None,
+        ds_config=None,
+        device_map=None,
+        packing_samples=False,
+        **kwargs,
+    ) -> None:
+        super().__init__()
+
+        if isinstance(pretrain_or_model, str):
+            attn_implementation = "flash_attention_2" if use_flash_attention_2 else "eager"
+
+            # Note: dschf is defined in function scope to avoid global effects
+            # https://huggingface.co/docs/transformers/deepspeed#non-trainer-deepspeed-integration
+            if ds_config is not None and ds_config["zero_optimization"]["stage"] == 3:
+                dschf = HfDeepSpeedConfig(ds_config)
+            else:
+                dschf = None
+
+            if load_in_4bit:
+                assert bf16, "we only support bnb_4bit_compute_dtype = bf16"
+                nf4_config = BitsAndBytesConfig(
+                    load_in_4bit=True,
+                    bnb_4bit_quant_type="nf4",
+                    bnb_4bit_use_double_quant=True,
+                    bnb_4bit_compute_dtype=torch.bfloat16,
+                )
+            else:
+                nf4_config = None
+                
+            self.model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
+                pretrain_or_model,
+                trust_remote_code=True,
+                attn_implementation=attn_implementation,
+                
